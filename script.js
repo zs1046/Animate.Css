689cf08215f735c245bbc8dd75f26a4be184eabb
Zach Sotak <zs1046@txstate.edu>
2018-06-01 16:54:29 -0500

    update list to handle events

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-var button= document.getElementsByTagName("button")[0];
+var button= document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
-button.addEventListener("click", function(){
-    console.log("CLICK!!!");
-})
+function inputLength(){
+    return input.value.length;
+}
+
+function createListElement(){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value= "";
+}
+
+function addListAfterClick(){
+    if(inputLength() > 0){
+        createListElement()
+    }
+}
+
+function addListAfterKeypress(event){
+    if(inputLength() > 0 && event.keyCode === 13){
+        createListElement()
+    }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
